e57765d3936b1c626f05a982be6b2865f81f068c
Chuliaen <julian.grassl@hotmail.de>
2021-02-15 17:23:53 +0100

    P5

--- src/edu/p05/A13.java
+++ src/edu/p05/A13.java
@@ -1,19 +1,21 @@
 package edu.p05;
 
+import edu.unibw.etti.array.ArrayVisualizer;
+
 import java.util.Arrays;
 
 public class A13 {
 
     public static void main(String[] args) {
-        double[] a = {1.0, 2.0, 3.0, 4.0, 5.0};
-        double[] b = {5.0, 4.0, 3.0, 2.0, 1.0};
-        System.out.println(Arrays.toString(selectionSort(b)));
+        int[] a = {5, 4, 3, 2, 1};
+        ArrayVisualizer.add(a);
+        System.out.println(Arrays.toString(selectionSort(a)));
     }
 
-    public static double[] selectionSort(double[] a) {
+    public static int[] selectionSort(int[] a) {
         int count;
-        double tmp;
-        double tmp2;
+        int tmp;
+        int tmp2;
 
         int vergleiche = 0;
         int vertauschungen = 0;
@@ -23,20 +25,51 @@ public class A13 {
             tmp2 = a[i];
             count = i;
 
-            for (int j = i; j < a.length; j++) {
+            for (int j = i + 1; j < a.length; j++) {
                 vergleiche++;
                 if (tmp > a[j]) {
                     tmp = a[j];
                     count = j;
                     vertauschungen++;
+
+                    ArrayVisualizer.update();
+                    ArrayVisualizer.setSleep(100);
                 }
             }
 
             a[i] = tmp;
             a[count] = tmp2;
+
+            ArrayVisualizer.update();
+            ArrayVisualizer.setSleep(100);
         }
         System.out.println("Vergleiche: " + vergleiche);
         System.out.println("Vertauschungen: " + vertauschungen);
         return a;
+    }
+
+    public static void direkteAuswahl(int[] x) {
+        int vergleiche = 0;
+        int vertausche = 0;
+
+        for (int sortiert = 0; sortiert < x.length; sortiert++) {
+            int min_index = sortiert;
+
+            for (int unsortiert = sortiert + 1; unsortiert < x.length; unsortiert++) {
+                vergleiche++;
+                if (x[unsortiert] < x[min_index]) {
+                    min_index = unsortiert;
+                }
+            }
+
+            if (min_index != sortiert) {
+                int hilf = x[sortiert];
+                x[sortiert] = x[min_index];
+                x[min_index] = hilf;
+                vertausche++;
+                ArrayVisualizer.update();
+                ArrayVisualizer.setSleep(100);
+            }
+        }
     }
 }
--- src/edu/p05/A15.java
+++ src/edu/p05/A15.java
@@ -1,4 +1,48 @@
 package edu.p05;
 
+import java.util.Arrays;
+
 public class A15 {
+
+    public static void main(String[] args) {
+        double[] x = {1.0, 3.0, 4.5, 2.3, 7.1, 1.2, 1.5, 3.0};
+        quickSort(x, 0, x.length - 1);
+        System.out.println(Arrays.toString(x));
+    }
+
+    public static void quickSort(double[] a, int i, int j) {
+        if (a == null || j <= i) {
+            return;
+        }
+        double pivot = a[i];
+        int vonLinks = i;
+        int vonRechts = j;
+        while (vonLinks <= vonRechts) {
+            while (a[vonLinks] > pivot) {
+                vonLinks++;
+                if (vonLinks > j) {
+                    break;
+                }
+            }
+            while (a[vonRechts] <= pivot) {
+                vonRechts--;
+                if (vonRechts < i) {
+                    break;
+                }
+            }
+            if (vonLinks < vonRechts) {
+                double tmp = a[vonLinks];
+                a[vonLinks] = a[vonRechts];
+                a[vonRechts] = tmp;
+            }
+        }
+        if (vonRechts < i) {
+// quickSort(a, i, i);
+            quickSort(a, i + 1, j);
+        } else {
+            quickSort(a, i, vonRechts);
+            quickSort(a, vonLinks, j);
+        }
+    }
+
 }
